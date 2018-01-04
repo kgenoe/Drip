1ae6a601b2f1c794c3d94b475cb45cc20c3a4b6c
Kyle <kyle@kyles-macbook-pro.local>
2018-01-04 16:40:06 -0500

    Create basic model for supported units of volume

---
+++ DripSimpleHydration/Model/UnitType.swift
@@ -0,0 +1,15 @@
+//
+//  UnitType.swift
+//  DripSimpleHydration
+//
+//  Created by Kyle on 2018-01-04.
+//  Copyright © 2018 Kyle. All rights reserved.
+//
+
+import Foundation
+
+enum UnitType: String {
+    case Metric
+    case Cups
+    case Ounces
+}
