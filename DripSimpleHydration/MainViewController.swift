a8d6486734742cf1f62f93e074c030eba4c14146
Kyle <kyle@kyles-macbook-pro.local>
2018-01-04 16:37:12 -0500

    Add basic view controller structure

---
+++ DripSimpleHydration/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  DripSimpleHydration
+//
+//  Created by Kyle on 2018-01-04.
+//  Copyright © 2018 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+
+
+}
